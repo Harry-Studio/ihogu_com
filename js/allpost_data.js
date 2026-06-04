e9ec8d736e31391fc7bddd4b8543cfdee22227ce
haoleiq@gmail.com <haoleiq@gmail.com>
2026-06-04 14:51:52 +0800

    Site updated: 2026-06-04 14:51:51

--- js/allpost_data.js
+++ js/allpost_data.js
@@ -58,21 +58,6 @@ window.BLOG_POSTS = [
     "excerpt": ""
   },
   {
-    "title": "Video Batch Frame Blending, Frame Interpolation, and Editing Automation Assistant",
-    "path": "p/video-batch-frame-blend-editing-assistant/",
-    "date": "2025-11-11",
-    "year": 2025,
-    "categories": [
-      "Windows Software"
-    ],
-    "tags": [
-      "IT Technology"
-    ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/video-batch-frame-blend-editing-assistant/0.jpg",
-    "description": null,
-    "excerpt": ""
-  },
-  {
     "title": "AI ID Photo Generation, Image Text Editing, One-Click Hairstyle Change",
     "path": "p/ai-id-photo-generation-text-edit-hairstyle/",
     "date": "2025-11-30",
@@ -84,6 +69,21 @@ window.BLOG_POSTS = [
       "IT Technology"
     ],
     "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/aizhengjianzhao/0.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "Video Batch Frame Blending, Frame Interpolation, and Editing Automation Assistant",
+    "path": "p/video-batch-frame-blend-editing-assistant/",
+    "date": "2025-11-11",
+    "year": 2025,
+    "categories": [
+      "Windows Software"
+    ],
+    "tags": [
+      "IT Technology"
+    ],
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/video-batch-frame-blend-editing-assistant/0.jpg",
     "description": null,
     "excerpt": ""
   },
@@ -119,22 +119,6 @@ window.BLOG_POSTS = [
     "excerpt": ""
   },
   {
-    "title": "Sales Management Assistant App",
-    "path": "p/sales-management-assistant-app/",
-    "date": "2016-08-22",
-    "year": 2016,
-    "categories": [
-      "APP"
-    ],
-    "tags": [
-      "IT Technology",
-      "Business Services"
-    ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/sales-management-assistant-app/1.png",
-    "description": "Sales Management Assistant App: app registration authorization/data backup/export/order management/customer management/product management",
-    "excerpt": ""
-  },
-  {
     "title": "Maoxiaoyi - CRM Customer Management System",
     "path": "p/maoxiaoyi-crm-customer-management/",
     "date": "2017-04-30",
@@ -151,18 +135,19 @@ window.BLOG_POSTS = [
     "excerpt": ""
   },
   {
-    "title": "VAT Tax Reduction Calculator",
-    "path": "p/vat-tax-reduction-calculator/",
-    "date": "2017-08-06",
-    "year": 2017,
+    "title": "Sales Management Assistant App",
+    "path": "p/sales-management-assistant-app/",
+    "date": "2016-08-22",
+    "year": 2016,
     "categories": [
-      "Mini Program"
+      "APP"
     ],
     "tags": [
-      "Finance"
+      "IT Technology",
+      "Business Services"
     ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/shuiwujisuan/shuiwujisuan.png",
-    "description": "VAT Tax Reduction Calculator",
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/sales-management-assistant-app/1.png",
+    "description": "Sales Management Assistant App: app registration authorization/data backup/export/order management/customer management/product management",
     "excerpt": ""
   },
   {
@@ -260,6 +245,21 @@ window.BLOG_POSTS = [
     "excerpt": ""
   },
   {
+    "title": "Albert LaCubeTV App-New Zealand",
+    "path": "p/albert-lacubetv-app-nz/",
+    "date": "2018-04-13",
+    "year": 2018,
+    "categories": [
+      "APP"
+    ],
+    "tags": [
+      "Social",
+      "Entertainment"
+    ],
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/ABGSYUK/ABGSYUK.png",
+    "excerpt": ""
+  },
+  {
     "title": "Flick Post-Malaysia",
     "path": "p/flick-post-malaysia/",
     "date": "2019-06-07",
@@ -273,21 +273,6 @@ window.BLOG_POSTS = [
     ],
     "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/flickpost/flickpost.png",
     "description": null,
-    "excerpt": ""
-  },
-  {
-    "title": "Albert LaCubeTV App-New Zealand",
-    "path": "p/albert-lacubetv-app-nz/",
-    "date": "2018-04-13",
-    "year": 2018,
-    "categories": [
-      "APP"
-    ],
-    "tags": [
-      "Social",
-      "Entertainment"
-    ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/ABGSYUK/ABGSYUK.png",
     "excerpt": ""
   },
   {
@@ -306,6 +291,21 @@ window.BLOG_POSTS = [
     "excerpt": ""
   },
   {
+    "title": "VAT Tax Reduction Calculator",
+    "path": "p/vat-tax-reduction-calculator/",
+    "date": "2017-08-06",
+    "year": 2017,
+    "categories": [
+      "Mini Program"
+    ],
+    "tags": [
+      "Finance"
+    ],
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/shuiwujisuan/shuiwujisuan.png",
+    "description": "VAT Tax Reduction Calculator",
+    "excerpt": ""
+  },
+  {
     "title": "Huanma Gas Station - Cryptocurrency Trends",
     "path": "p/huanma-gas-station-crypto-trend/",
     "date": "2019-12-01",
@@ -314,8 +314,8 @@ window.BLOG_POSTS = [
       "Mini Program"
     ],
     "tags": [
-      "Finance",
-      "News"
+      "News",
+      "Finance"
     ],
     "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/paomaJiaYou/paomaJiaYou.png",
     "description": null,
@@ -354,6 +354,22 @@ window.BLOG_POSTS = [
     "excerpt": ""
   },
   {
+    "title": "Garbage Sorting WeChat Mini Game",
+    "path": "p/garbage-sorting-wechat-mini-game/",
+    "date": "2020-07-10",
+    "year": 2020,
+    "categories": [
+      "Mini Game"
+    ],
+    "tags": [
+      "Entertainment",
+      "Lifestyle Services"
+    ],
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/lajifenlei/lajifenlei.png",
+    "description": "Learn garbage sorting through a WeChat falling-object mini game",
+    "excerpt": ""
+  },
+  {
     "title": "Wedding Encyclopedia Q&A",
     "path": "p/wedding-encyclopedia-qa/",
     "date": "2021-09-01",
@@ -366,68 +382,6 @@ window.BLOG_POSTS = [
       "Lifestyle Services"
     ],
     "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/steven/steven.png",
-    "description": null,
-    "excerpt": ""
-  },
-  {
-    "title": "Xinshichuang Home Services",
-    "path": "p/xinshichuang-home-services/",
-    "date": "2021-10-01",
-    "year": 2021,
-    "categories": [
-      "APP"
-    ],
-    "tags": [
-      "Merchant-Owned",
-      "Lifestyle Services"
-    ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/SgXinShiChuang/SgXinShiChuang.png",
-    "description": null,
-    "excerpt": ""
-  },
-  {
-    "title": "Outing Travel Lite",
-    "path": "p/outing-travel-lite/",
-    "date": "2022-01-01",
-    "year": 2022,
-    "categories": [
-      "Mini Program"
-    ],
-    "tags": [
-      "Social",
-      "Travel Services"
-    ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/outing/outing.png",
-    "excerpt": ""
-  },
-  {
-    "title": "Scan-to-Order",
-    "path": "p/scan-to-order/",
-    "date": "2022-02-03",
-    "year": 2022,
-    "categories": [
-      "Mini Program"
-    ],
-    "tags": [
-      "Merchant-Owned",
-      "Catering Services"
-    ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/Ordering/Ordering.png",
-    "excerpt": ""
-  },
-  {
-    "title": "Scan-to-Order Smart Store - Promoter",
-    "path": "p/scan-order-smart-store-promoter/",
-    "date": "2023-02-01",
-    "year": 2023,
-    "categories": [
-      "APP"
-    ],
-    "tags": [
-      "Business Services",
-      "Merchant-Owned"
-    ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/yu_le_dui_spread/0.png",
     "description": null,
     "excerpt": ""
   },
@@ -447,6 +401,53 @@ window.BLOG_POSTS = [
     "excerpt": ""
   },
   {
+    "title": "Xinshichuang Home Services",
+    "path": "p/xinshichuang-home-services/",
+    "date": "2021-10-01",
+    "year": 2021,
+    "categories": [
+      "APP"
+    ],
+    "tags": [
+      "Merchant-Owned",
+      "Lifestyle Services"
+    ],
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/SgXinShiChuang/SgXinShiChuang.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "Chip Wholesale Website",
+    "path": "p/chip-wholesale-website/",
+    "date": "2022-01-07",
+    "year": 2022,
+    "categories": [
+      "Website"
+    ],
+    "tags": [
+      "Chips"
+    ],
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/shop_xinpian_shenzhen/xinpian1.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "Scan-to-Order Smart Store - Promoter",
+    "path": "p/scan-order-smart-store-promoter/",
+    "date": "2023-02-01",
+    "year": 2023,
+    "categories": [
+      "APP"
+    ],
+    "tags": [
+      "Business Services",
+      "Merchant-Owned"
+    ],
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/yu_le_dui_spread/0.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
     "title": "Scan-to-Order Smart Store - Merchant Side",
     "path": "p/scan-order-smart-store-merchant/",
     "date": "2023-04-01",
@@ -463,22 +464,6 @@ window.BLOG_POSTS = [
     "excerpt": ""
   },
   {
-    "title": "Scan-to-Order Smart Store - User Side",
-    "path": "p/scan-order-smart-store-user/",
-    "date": "2023-04-01",
-    "year": 2023,
-    "categories": [
-      "Mini Program"
-    ],
-    "tags": [
-      "Merchant-Owned",
-      "Lifestyle Services"
-    ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/scan-order-smart-store-user/0.png",
-    "description": null,
-    "excerpt": ""
-  },
-  {
     "title": "Video Subtitle AI Assistant",
     "path": "p/video-subtitle-ai-helper/",
     "date": "2024-03-01",
@@ -489,7 +474,7 @@ window.BLOG_POSTS = [
     "tags": [
       "IT Technology"
     ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/shipin_zhuan_wenzi/0.svg",
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/shipin_zhuan_wenzi/0.png",
     "description": null,
     "excerpt": ""
   },
@@ -525,6 +510,52 @@ window.BLOG_POSTS = [
     "excerpt": ""
   },
   {
+    "title": "Scan-to-Order",
+    "path": "p/scan-to-order/",
+    "date": "2022-02-03",
+    "year": 2022,
+    "categories": [
+      "Mini Program"
+    ],
+    "tags": [
+      "Merchant-Owned",
+      "Catering Services"
+    ],
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/Ordering/Ordering.png",
+    "excerpt": ""
+  },
+  {
+    "title": "Scan-to-Order Smart Store - User Side",
+    "path": "p/scan-order-smart-store-user/",
+    "date": "2023-04-01",
+    "year": 2023,
+    "categories": [
+      "Mini Program"
+    ],
+    "tags": [
+      "Merchant-Owned",
+      "Lifestyle Services"
+    ],
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/scan-order-smart-store-user/0.png",
+    "description": null,
+    "excerpt": ""
+  },
+  {
+    "title": "Outing Travel Lite",
+    "path": "p/outing-travel-lite/",
+    "date": "2022-01-01",
+    "year": 2022,
+    "categories": [
+      "Mini Program"
+    ],
+    "tags": [
+      "Social",
+      "Travel Services"
+    ],
+    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/outing/outing.png",
+    "excerpt": ""
+  },
+  {
     "title": "AI Clone Digital Human, Digital Life",
     "path": "p/ai-clone-digital-human-digital-life/",
     "date": "2025-10-30",
@@ -537,37 +568,6 @@ window.BLOG_POSTS = [
       "AI Model"
     ],
     "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/shuziren/0.png",
-    "description": null,
-    "excerpt": ""
-  },
-  {
-    "title": "Garbage Sorting WeChat Mini Game",
-    "path": "p/garbage-sorting-wechat-mini-game/",
-    "date": "2020-07-10",
-    "year": 2020,
-    "categories": [
-      "Mini Game"
-    ],
-    "tags": [
-      "Entertainment",
-      "Lifestyle Services"
-    ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/lajifenlei/lajifenlei.png",
-    "description": "Learn garbage sorting through a WeChat falling-object mini game",
-    "excerpt": ""
-  },
-  {
-    "title": "Chip Wholesale Website",
-    "path": "p/chip-wholesale-website/",
-    "date": "2022-01-07",
-    "year": 2022,
-    "categories": [
-      "Website"
-    ],
-    "tags": [
-      "Chips"
-    ],
-    "cover": "https://ihogu-com-1251903635.cos.na-siliconvalley.myqcloud.com/cos/work/shop_xinpian_shenzhen/xinpian1.png",
     "description": null,
     "excerpt": ""
   }
